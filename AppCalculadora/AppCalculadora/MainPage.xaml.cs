3c5eec7188d04f7e5f0ec0f66b57f8a37461cdb3
MARCOS PAULO MARTINS DE OLIVEIRA <marcos.pmoliveira1@senacsp.edu.br>
2019-09-16 17:33:20 -0300

    Adicionar arquivos de projeto.

---
+++ AppCalculadora/AppCalculadora/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCalculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn7_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn8_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn9_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSomar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn6_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn5_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn4_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnMultiplicar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn3_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn2_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn1_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSubtrair_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn0_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnLimpar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnIgual_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnDividir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
