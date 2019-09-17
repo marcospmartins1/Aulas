d0c24917ea3da7f119e2bd9afa7cb6644e2f60ca
MARCOS PAULO MARTINS DE OLIVEIRA <marcos.pmoliveira1@senacsp.edu.br>
2019-09-17 17:11:17 -0300

    Primeira modificação nos códigos

--- AppCalculadora/AppCalculadora/MainPage.xaml.cs
+++ AppCalculadora/AppCalculadora/MainPage.xaml.cs
@@ -16,55 +16,60 @@ namespace AppCalculadora
 
         private void Btn7_Clicked(object sender, EventArgs e)
         {
-
+            entryNumero.Text += "7";
         }
 
         private void Btn8_Clicked(object sender, EventArgs e)
         {
-
+            entryNumero.Text += "8";
         }
 
         private void Btn9_Clicked(object sender, EventArgs e)
         {
-
-        }
-
-        private void BtnSomar_Clicked(object sender, EventArgs e)
-        {
-
+            entryNumero.Text += "9";
         }
 
         private void Btn6_Clicked(object sender, EventArgs e)
         {
-
+            entryNumero.Text += "6";
         }
 
         private void Btn5_Clicked(object sender, EventArgs e)
         {
-
+            entryNumero.Text += "5";
         }
 
         private void Btn4_Clicked(object sender, EventArgs e)
         {
-
-        }
-
-        private void BtnMultiplicar_Clicked(object sender, EventArgs e)
-        {
-
+            entryNumero.Text += "4";
         }
 
         private void Btn3_Clicked(object sender, EventArgs e)
         {
+            entryNumero.Text += "3";
 
         }
 
         private void Btn2_Clicked(object sender, EventArgs e)
         {
-
+            entryNumero.Text += "2";
         }
 
         private void Btn1_Clicked(object sender, EventArgs e)
+        {
+            entryNumero.Text += "1";
+        }
+
+        private void Btn0_Clicked(object sender, EventArgs e)
+        {
+            entryNumero.Text += "0";
+        }
+
+        private void BtnIgual_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void BtnSomar_Clicked(object sender, EventArgs e)
         {
 
         }
@@ -74,22 +79,17 @@ namespace AppCalculadora
 
         }
 
-        private void Btn0_Clicked(object sender, EventArgs e)
-        {
-
-        }
-
-        private void BtnLimpar_Clicked(object sender, EventArgs e)
-        {
-
-        }
-
-        private void BtnIgual_Clicked(object sender, EventArgs e)
+        private void BtnMultiplicar_Clicked(object sender, EventArgs e)
         {
 
         }
 
         private void BtnDividir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnLimpar_Clicked_1(object sender, EventArgs e)
         {
 
         }
